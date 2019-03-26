20bb8a865f16c62d80335c0c64fba3584bdb2146
LaercioNazareno <laercio.nazareno1@gmail.com>
2019-03-26 17:35:48 -0300

    Cadastro

---
+++ GNI/front-end/app/src/main/java/com/gerenciador_negocios_integrado/app/Connection.java
@@ -0,0 +1,32 @@
+package com.gerenciador_negocios_integrado.app;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class Connection extends SQLiteOpenHelper {
+
+    private static final String name = "gni_db";
+    private static final int version = 1;
+
+
+    public Connection(Context context) {
+        super(context, name, null, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL("create table user(id integer primary key autoincrement;" +
+                                     "email varchar(50); name varchar(50); " +
+                                     "last_name varchar(50); " +
+                                     "password varchar(10); )");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+}
